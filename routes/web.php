0721c3ccfcb8a58aa09032d066482b5dab4b8da7
IWAN SAFII <you@example.com>
2026-05-07 16:14:06 +0700

    0.1.4

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
 Route::middleware('guest')->group(function () {
